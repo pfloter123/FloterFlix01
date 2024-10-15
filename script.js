5a3691f52bef51f5b0e240fe36aadd03cc38bd5a
pfloter123 <pedrooliveirafloter0@gmail.com>
2024-10-14 22:58:48 -0300

    floterFlix_Pedro

--- script.js
+++ script.js
@@ -1,5 +1,1 @@
-// Espera 3 segundos antes de redirecionar
-setTimeout(function() {
-    // Redireciona para a página desejada
-    window.location.href = 'tela_inicial.html';
-}, 3000); // 3000 milissegundos = 3 segundos
+setTimeout(function(){window.location.href="tela_inicial.html"},3e3);
